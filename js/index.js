0d50a708f9b91ea51bedeabe0906519f40773a28
Aneeta M <aneetamaryva@gmail.com>
2023-12-13 14:47:28 -0500

    scroll feature and some makeup page changes

--- js/index.js
+++ js/index.js
@@ -33,3 +33,23 @@ if(sessionStorage.getItem('username')){
 else{
 	mySpan.style.display = 'none';
 }
+
+
+// scroll to top button function
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+scrollToTopBtn.addEventListener("click", function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+});
